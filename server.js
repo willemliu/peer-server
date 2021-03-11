8ceddcdfed3e0835d5589bd77bc33d04b034cae7
Willem Liu <willem_liu@hotmail.com>
2021-03-11 14:10:51 +0100

    Added some logging

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const { PeerServer } = require("peer");
 const port = process.env.PORT || 443;
-PeerServer({ port, path: "/", proxied: true });
+const peerServer = PeerServer({ port, path: "/", proxied: true });
+console.log(`Serving on ${port}`);
+let clients = [];
+peerServer.on("connection", (client) => {
+    clients.push({ id: client.getId(), client });
+    console.log(`Connections ${clients.length}`);
+});
+
+peerServer.on("disconnect", (client) => {
+    clients = clients.filter((_client) => _client.id !== client.getId());
+    console.log(`Connections ${clients.length}`);
+});
